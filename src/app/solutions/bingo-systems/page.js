270cce3f2b44f60c8a5035b80c408ec278c5c241
Ben Smith <smithbennettlarue@gmail.com>
2024-06-26 12:22:29 -0400

    Added 'online-lotter' and 'bingo-systems' pages to 'Solutions' section

---
+++ src/app/solutions/bingo-systems/page.js
@@ -0,0 +1,121 @@
+"use client";
+
+import { ArrowButton } from "@/app/components/ArrowButton";
+import { Footer } from "@/app/components/Footer";
+import { Nav } from "@/app/components/Nav";
+import MachineLink from "@/app/components/home/MachineLink";
+import { Title } from "@/app/components/products/Title";
+
+const checks = [
+    "Standard bingo/keno models are Galaxy, SuperBingo and Phoenix II Bingo",
+    "Other models such as Criterion, Saturn have been modified for Bingo use",
+    "Automatic operation with our SmartController and ConnectServer Software",
+    "Add optional cameras and monitors",
+    "Add the Solution Controller and manage the continuous operation of your Bingo or Keno game remotely from a web browser.",
+];
+
+const data = [
+    {
+        title: "Galaxy Bingo",
+        desc: "A Feature-Rich, Yet Economical Bingo Machine",
+        imageLink:
+            "https://smartplay.com/wp-content/uploads/2017/08/Galaxy-bingo-machine-435x895.jpg",
+    },
+    {
+        title: "Phoenix Bingo Machine",
+        desc: "The Phoenix keno/bingo draw machine is suitable for all lotto / keno / bingo game matrices.",
+        imageLink:
+            "https://smartplay.com/wp-content/uploads/2017/07/phoenix-lottery-machine-guatemala.jpg",
+    },
+    {
+        title: "Criterion II Lottery Draw Machine",
+        desc: "A Unique Visual with Dual Mixing Arms Moving in Opposite Directions",
+        imageLink:
+            "https://smartplay.com/wp-content/uploads/2017/07/criterion-lottery-ireland.jpg",
+    },
+];
+
+export default function App() {
+    return (
+        <div className="App">
+            <Nav />
+            <div className="fixed top-0 left-0 w-full h-full -z-10">
+                <img
+                    src="https://hxl.550.myftpupload.com/wp-content/uploads/2021/10/globe.jpg"
+                    alt="Customer Service"
+                    className="w-full h-full object-cover -translate-y-10"
+                />
+                <div className="absolute top-0 left-0 w-full h-full bg-white opacity-50"></div>
+            </div>
+
+            <Title text="Bingo Systems" />
+            <div className="bg-white">
+                <div className="mx-auto max-w-[1100px] pt-20 pb-12 text-lg grid">
+                    <div className="space-y-10">
+                        <h2 className="text-3xl font-semibold text-blue-950 text-center">
+                            Highest quality parts and construction to meet the
+                            demands of continuous operation
+                        </h2>
+                        <p className="text-lg text-center px-10">
+                            Bingo and keno games are popular throughout the
+                            world. Both game styles require all balls to be
+                            displayed one after the other. Smartplay has several
+                            machines designed specifically for bingo and keno.
+                            Some of our traditional lotto-style machines have
+                            been adapted to bingo and keno as well. Machines can
+                            accommodate standard 75 and 90-ball bingo and we can
+                            create custom machines to meet your requirements.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-200 py-12">
+                <div className="grid grid-cols-2 mx-auto max-w-[1100px] gap-x-6">
+                    <ul className="space-y-6 bg-gray-200 p-4 text-blue-950 font-semibold col-span-1">
+                        {checks.map((check, index) => (
+                            <li key={index} className="flex space-x-2 text-lg">
+                                <p className="text-xl">•</p>
+                                <p>{check}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="col-span-1 flex justify-center items-center space-x-4 bg-white p-5 rounded shadow">
+                        <img
+                            src="https://smartplay.com/wp-content/uploads/2017/07/super-bingo-machine.jpg"
+                            alt="Online Lottery"
+                            className="rounded shadow h-[360px]"
+                        />
+                        <div>
+                            <h2 className="text-2xl font-semibold text-blue-950">
+                                SuperBingo
+                            </h2>
+                            <p className="py-5">
+                                This bingo machine is designed for
+                                high-frequency use, drawing balls every few
+                                minutes.
+                            </p>
+                            <ArrowButton text="Learn More" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white py-12">
+                <h2 className="text-2xl font-semibold text-blue-950 text-center mb-12">
+                    Standard Bingo and Keno Models Used Around the World
+                </h2>
+                <div className="grid grid-cols-3 mx-auto max-w-[1100px] gap-x-6">
+                    {data.map((item, index) => (
+                        <MachineLink
+                            key={index}
+                            title={item.title}
+                            desc={item.desc}
+                            imageLink={item.imageLink}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
---
+++ src/app/solutions/online-lottery/page.js
@@ -0,0 +1,72 @@
+"use client";
+
+import { Footer } from "@/app/components/Footer";
+import { Nav } from "@/app/components/Nav";
+import { Title } from "@/app/components/products/Title";
+
+const checks = [
+    "High-quality, machines designed for continuous play",
+    "Extended maintenance plans for years of hassle-free operation",
+    "SQL ConnectServer automation software to remotely manage the entire draw process",
+    "DrawStudio enclosures to create a secure, self-contained drawing environment",
+];
+
+export default function App() {
+    return (
+        <div className="App">
+            <Nav />
+            <div className="fixed top-0 left-0 w-full h-full -z-10">
+                <img
+                    src="https://hxl.550.myftpupload.com/wp-content/uploads/2021/10/globe.jpg"
+                    alt="Customer Service"
+                    className="w-full h-full object-cover -translate-y-10"
+                />
+                <div className="absolute top-0 left-0 w-full h-full bg-white opacity-50"></div>
+            </div>
+
+            <Title text="Online Lottery" />
+            <div className="bg-white">
+                <div className="mx-auto max-w-[1100px] pt-20 pb-12 text-lg grid">
+                    <div className="space-y-10">
+                        <h2 className="text-3xl font-semibold text-blue-950 text-center">
+                            Successful and hassle-free online lotteries require
+                            the highest-quality equipment and the latest
+                            technology.
+                        </h2>
+                        <p className="text-lg text-center px-10">
+                            Online lotteries that operate continuously require
+                            high-quality equipment that can operate flawlessly
+                            under frequent use. You also need the proper support
+                            to provide the routine maintenance required for
+                            years of hassle-free operation. Smartplay has worked
+                            with online lotteries and betting sites since 2011.
+                            We can help you match the right lottery system for
+                            your game type and theme for maximum visual impact
+                            and reliability.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-200 py-12">
+                <div className="grid grid-cols-2 mx-auto max-w-[1100px] gap-x-6">
+                    <ul className="space-y-6 bg-gray-200 p-4 text-blue-950 font-semibold col-span-1">
+                        {checks.map((check, index) => (
+                            <li key={index} className="flex space-x-2 text-lg">
+                                <p className="text-xl">✓</p>
+                                <p>{check}</p>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <img
+                        src="https://smartplay.com/wp-content/uploads/2017/06/betgames1.jpg"
+                        alt="Online Lottery"
+                        className="col-span-1 rounded shadow"
+                    />
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
